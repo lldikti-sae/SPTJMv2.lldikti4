3b66e513069f0b87daea4e82dddd5bd76e570b4a
Arya <aryawahyupratama245@gmail.com>
2026-06-04 07:39:40 +0700

    hemm

--- scratch_test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-$app = require_once 'bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-$k = \Illuminate\Support\Facades\DB::table('t_kekurangan')->where('nidn', '10036806')->get();
-foreach($k as $r) {
-    echo $r->jenis_pembayaran . " -> " . $r->selisih . "\n";
-}
