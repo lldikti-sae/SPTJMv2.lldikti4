4be648bde3e3a15a0877794942b0453ce5a59583
Arya <aryawahyupratama245@gmail.com>
2026-07-13 10:18:45 +0700

    menghapus file percobaan

--- test_grade.php
+++
@@ -1,9 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
-
-$grades = DB::table('c_grade_serdos')->get();
-foreach($grades as $g) {
-    echo $g->jabatan . " | " . $g->golongan . " | " . $g->masa_kerja_bawah . " - " . $g->masa_kerja_atas . "\n";
-}
